7d85c931259905eea345ece6783c287de2ed116b
MarkB137 <98420296+MarkB137@users.noreply.github.com>
2022-03-31 14:06:53 -0400

    Add files via upload

---
+++ WA120_Ch9_tutorial/script9.js
@@ -0,0 +1,9 @@
+// script for Ch. 9 practice project
+"use strict";
+
+document.getElementById("bottom").textContent = "This is the end of this document.";
+
+document.getElementsByClassName("main")[1].innerHTML = "Here is my paragraph. <br/> <h3>Don't you love it?</h3>";
+
+document.getElementByTagName("h1")[0].style.color = "green";
+document.getElementByTagName("h1")[0].style.fontSize = "50px";
